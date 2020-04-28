a0508f4cc83986ed52fe43466a49832d94d7bcb1
nathandoesmuch <47115079+nathandoesmuch@users.noreply.github.com>
2020-04-27 22:41:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # coding=utf-8
-from flask import Flask, render_template
-from views.nathandoesmuch import nathandoesmuch
-from views.victoriashakes import victoriashakes
-from views.tybarr import tybarr
-from views.dayviendunlap import dayviendunlap
+from flask import Flask, render_template, request
 
-app = Flask(__name__, host_matching=True, static_host='nathandoesmuch.com')
+app = Flask(__name__)
 
-app.register_blueprint(nathandoesmuch, host='nathandoesmuch.com')
-app.register_blueprint(victoriashakes, host='victoriashakes.com')
-app.register_blueprint(tybarr, host='tybarr.com')
-app.register_blueprint(dayviendunlap, host='dayviendunlap.com')
+@app.route("/")
+def index():
+    if request.host == 'nathandoesmuch.com':
+        return render_template("nathandoesmuch.html")
+    elif request.host == 'victoriashakes.com':
+        return "Hello, Victoria!"
+    elif request.host == 'tybarr.com.com':
+        return "Hello, Ty!"
+    elif request.host == 'dayviendunlap.com':
+        return "Hello, Dayvien!"
+    else:
+        return "Hello, Other users!"
 
 if __name__ == "__main__":
     app.run()
