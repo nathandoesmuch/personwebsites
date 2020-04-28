3976ee18b92422ecc7b67d8098053f447b76dfdc
nathandoesmuch <47115079+nathandoesmuch@users.noreply.github.com>
2020-04-27 22:41:46 -0500

    Delete nathandoesmuch.py

--- views/nathandoesmuch.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-nathandoesmuch = Blueprint('nathandoesmuch', __name__)
-
-@nathandoesmuch.route("/")
-def index():
-    return render_template('nathandoesmuch.html')
